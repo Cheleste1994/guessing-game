b76fdcc53bcce62dc38817d72ef62ac3465685a5
Cheleste1994 <wolverines@tut.by>
2023-02-22 16:52:38 +0300

    refactor: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.a = Math.ceil((this.max - this.min) / 2) + this.min
+       
+        return this.a
     }
 
     lower() {
-
+        return this.max = this.a;
     }
 
     greater() {
-
+       return this.min = this.a;
     }
 }
 
 module.exports = GuessingGame;
+
+
+
